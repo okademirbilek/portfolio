c8f283a01e52e4b1277794d8c2bf636c2b9f851e
Okan Demirbilek <48480726+okademirbilek@users.noreply.github.com>
2022-11-30 21:13:33 +0300

    sss

---
+++ projects.js
@@ -0,0 +1,34 @@
+export default {
+    "data": {
+        "projects": [
+            {
+                "id": "1",
+                "name": "Threejs Portfolio website",
+                "url": "https://github.com/okademirbilek/lostarkimages/blob/main/threejsimg.PNG?raw=true",
+                "url2": "https://github.com/okademirbilek/dontdismantleit",
+                "exp":"İts a 3D interactive portfolio website.Built using with JavaScript Threejs and Blender ",
+                "width": 600,
+                "height": 908
+            },
+            {
+                "id": "2",
+                "name": "Item Founder for LostArk Game",
+                "url": "https://github.com/okademirbilek/lostarkimages/blob/main/lostark.PNG?raw=true",
+                "url2": "https://github.com/okademirbilek/dontdismantleit",
+                "exp":"Its a website for Amazon's LostArk game.It finds which items belongs to which class.I didn't know much about html css and javascript when I started this site, I just put it to see where I got from the first website I made.",
+                "width": 600,
+                "height": 908
+            },
+            {
+                "id": "3",
+                "name": "Blackjack Game",
+                "url": "https://github.com/okademirbilek/lostarkimages/blob/main/blackjack.PNG?raw=true",
+                "url2": "https://github.com/okademirbilek/blackjack",
+                "exp":"Its an scrimba solo project blackjack game.I continue to develop as I have time",
+                "width": 600,
+                "height": 908
+            },
+            
+        ]
+    }
+}
