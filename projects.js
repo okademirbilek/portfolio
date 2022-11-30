17fab8e8fe43af034480ab9122d323308a6d8395
Okan Demirbilek <48480726+okademirbilek@users.noreply.github.com>
2022-11-30 22:36:01 +0300

    Update projects.js

--- projects.js
+++ projects.js
@@ -5,7 +5,7 @@ export default {
                 "id": "1",
                 "name": "Threejs Portfolio website",
                 "url": "https://github.com/okademirbilek/lostarkimages/blob/main/threejsimg.PNG?raw=true",
-                "url2": "https://github.com/okademirbilek/dontdismantleit",
+                "url2": "https://github.com/okademirbilek/portfolio",
                 "exp":"İts a 3D interactive portfolio website.Built using with JavaScript Threejs and Blender ",
                 "width": 600,
                 "height": 908
