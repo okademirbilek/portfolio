99948d578a0892b94a98028bf6bd93ab0fb36ba6
Okan Demirbilek <48480726+okademirbilek@users.noreply.github.com>
2022-12-08 01:51:26 +0300

    Update projects.js

--- projects.js
+++ projects.js
@@ -6,7 +6,7 @@ export default {
                 "name": "Threejs Portfolio Website",
                 "url": "https://github.com/okademirbilek/lostarkimages/blob/main/threejsimg.PNG?raw=true",
                 "url2": "https://github.com/okademirbilek/portfolio",
-                "exp":"Its a 3D interactive portfolio website.Built using with JavaScript Threejs and Blender ",
+                "exp":"It's a 3D interactive portfolio website.Built using with JavaScript Threejs and Blender ",
                 "width": 600,
                 "height": 908
             },
@@ -15,7 +15,7 @@ export default {
                 "name": "Item Founder for LostArk Game",
                 "url": "https://github.com/okademirbilek/lostarkimages/blob/main/lostark.PNG?raw=true",
                 "url2": "https://github.com/okademirbilek/dontdismantleit",
-                "exp":"Its a website for Amazon's LostArk game.It finds which items belongs to which class.I didn't know much about html css and javascript when I started this site, I just put it to see where I got from the first website I made.",
+                "exp":"It's a website for Amazon's LostArk game.It finds which items belongs to which class.I didn't know much about html css and javascript when I started this site, I just put it to see where I got from the first website I made.",
                 "width": 600,
                 "height": 908
             },
@@ -24,10 +24,19 @@ export default {
                 "name": "Blackjack Game",
                 "url": "https://github.com/okademirbilek/lostarkimages/blob/main/blackjack.PNG?raw=true",
                 "url2": "https://github.com/okademirbilek/blackjack",
-                "exp":"Its an scrimba solo project blackjack game.I continue to develop as I have time",
+                "exp":"It's a scrimba solo project blackjack game.I continue to develop as I have time",
                 "width": 600,
                 "height": 908
             },
+            {
+                "id": "4",
+                "name": "Tenzies Game",
+                "url": "https://github.com/okademirbilek/lostarkimages/blob/main/tenziesgame.JPG?raw=true",
+                "url2": "https://github.com/okademirbilek/TenziesGame",
+                "exp":"It's a react practice game called Tenzies Game ",
+                "width": 600,
+                "height": 908
+            }
             
         ]
     }
