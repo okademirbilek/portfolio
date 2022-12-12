0884a7fd5df39eaeecc6de2e37209b9c067a2aa3
Okan Demirbilek <48480726+okademirbilek@users.noreply.github.com>
2022-12-12 17:08:02 +0300

    Update projects.js

--- projects.js
+++ projects.js
@@ -12,10 +12,10 @@ export default {
             },
             {
                 "id": "2",
-                "name": "Item Founder for LostArk Game",
-                "url": "https://github.com/okademirbilek/lostarkimages/blob/main/lostark.PNG?raw=true",
-                "url2": "https://github.com/okademirbilek/dontdismantleit",
-                "exp":"It's a website for Amazon's LostArk game.It finds which items belongs to which class.I didn't know much about html css and javascript when I started this site, I just put it to see where I got from the first website I made.",
+                "name": "Quizzica app with react",
+                "url": "https://github.com/okademirbilek/lostarkimages/blob/main/quizzica.JPG?raw=true",
+                "url2": "https://github.com/okademirbilek/QuizGame",
+                "exp":"It's a scrimba solo project Quizzica.After answering the questions in the category you chose, you can see how many of them were correct.It gets the API data from the Open Trivia database.",
                 "width": 600,
                 "height": 908
             },
